6d387f0aee4d0a019ea422ee9210549a3740f8aa
rachimel <2023180014@tukorea.ac.kr>
2026-09-22 16:01:51 +0900

    물체의 위치 정보를 저장할 Vec2 클래스 선언 및 기존 Object 안에 있던 위치 정보 관련 데이터가 Vec2를 이용하도록 변경

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,14 +1,22 @@
 from pico2d import *
 
+# 아 맨날 x,y 넣는거 귀찮은데
+class Vec2:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
 # 오브젝트를 일단 선언할까?
 class Object:
     def __init__(self):
-        self.x = 0;
-        self.y = 0;
+        self.pos = Vec2()
+
 
 # 실행 함수
 def run():
     open_canvas()
+    # 오브젝트를 만들자
+    character = Object()
     while True:
         handle_events()
     close_canvas()
