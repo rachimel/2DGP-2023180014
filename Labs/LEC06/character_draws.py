e4eb90f583e689affc95a8e88890b3bbe21329dd
rachimel <2023180014@tukorea.ac.kr>
2026-09-22 15:45:03 +0900

    실행 함수 안으로 실제 게임 루프의 골격을 집어넣자...

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -2,18 +2,17 @@ from pico2d import *
 
 # 실행 함수
 def run():
-    pass
+    open_canvas()
+    while True:
+        handle_events()
+    close_canvas()
+
 # SDL 이벤트 루프
 def handle_events():
     events = get_events()
     for event in events:
         if(event.type == SDL_QUIT):
             exit()
-
-open_canvas()
-while True:
-    handle_events()
-close_canvas()
 
 if __name__ == "__main__":
     run()
